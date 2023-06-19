2a38205740847d5007bf8f10a05b261caa862b8d
HeitorGoncalves <heitor-goncalves@hotmail.com>
2023-06-19 20:56:45 -0300

    Novo projeto

---
+++ src/main/java/vaiagendar/VaiagendarApplication.java
@@ -0,0 +1,13 @@
+package vaiagendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaiagendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaiagendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vaiagendar/VaiagendarApplicationTests.java
@@ -0,0 +1,13 @@
+package vaiagendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaiagendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
